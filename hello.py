175d4b05d989a2f3d56a71206b3feb223db7f45d
Ivan Simeyko <iv.simeyko@gmail.com>
2016-03-11 22:15:41 +0200

    change configs

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
-bind = "0.0.0.0:8080"
 
-
-def app(environ, start_response):
+def wsgi_application(environ, start_response):
     status = '200 OK'
-    response_headers = [('Content-Type', 'text/plain')]
-    start_response(status, response_headers)
-    resp = '\r\n'.join(environ['QUERY_STRING'].split("&"))
-    return resp
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+
+    body = environ['QUERY_STRING'].replace('&', '\r\n')
+
+    start_response(status, headers)
+    return [body]
